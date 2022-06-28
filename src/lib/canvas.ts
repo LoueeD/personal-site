a0855d5f600315c19bcf09fbd3f4af347a2a1827
lou <loueed@gmail.com>
2022-06-29 00:19:15 +0100

    added wheel listener and experimental audio

---
+++ src/lib/canvas.ts
@@ -0,0 +1,127 @@
+class CanvasHelper {
+  canvas: HTMLCanvasElement;
+  ctx: CanvasRenderingContext2D;
+  mousePosition = { x: 0, y: 0 };
+  isMouseDown = false;
+  ticker = 0;
+  canvasRotate = Math.PI / 10;
+  nodes: {
+    speed: number;
+    size: number;
+    ordinal: number;
+    rotation: number;
+    x: number;
+    y: number;
+  }[] = [];
+  volumeMeterValue = 0;
+  wheelValue = 1;
+
+  constructor(canvas: HTMLCanvasElement) {
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext('2d') as CanvasRenderingContext2D;
+    this.setup();
+  }
+
+  setup() {
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+    this.nodes = [...new Array(4000)].map((_, i) => this.addNode(i));
+    this.draw();
+    window.addEventListener('mousemove', (e) => this.move(e));
+    window.addEventListener('touchmove', (e) => this.move(e));
+    window.addEventListener('wheel', (e) => {
+      this.wheelValue += (e.deltaY / 1000);
+      console.log(this.wheelValue);
+    });
+    // this.setupAudio();
+  }
+
+  move(e: MouseEvent | TouchEvent) {
+    const mouseEvent = e as MouseEvent;
+    if (mouseEvent.clientX && mouseEvent.clientY) {
+      const { clientX: x, clientY: y } = mouseEvent;
+      this.mousePosition = { x, y };
+      this.isMouseDown = mouseEvent.buttons !== 0;
+    } else {
+      e.preventDefault();
+      const touchEvent = e as TouchEvent;
+      const { clientX: x, clientY: y } = touchEvent.touches[0];
+      this.mousePosition = { x, y };
+    }
+  };
+
+  draw() {
+    const { x, y } = this.mousePosition;
+    if (this.ctx && this.ticker < 1) {
+      this.ticker += 0.05;
+      this.ctx.globalAlpha = this.ticker;
+    }
+    this.ctx.resetTransform();
+    this.ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    this.nodes.forEach((node, i) => {
+      if (this.ctx) {
+
+        this.ctx.beginPath();
+        this.ctx.arc(node.x, node.y, node.size, 0, 2 * Math.PI);
+        node.ordinal += 0.25;
+        this.ctx.fillStyle = this.makeColor(node.ordinal, 20);
+        const speed = node.speed; // += (Math.round(this.volumeMeterValue * 10));
+        this.ctx.fill();
+        const rand = Math.round(Math.max(1, Math.floor(Math.random() * this.wheelValue)));
+        if (node.x > x) node.x -= ((node.x / node.rotation) * speed) * rand;
+        if (node.x < x) node.x += ((node.x / node.rotation) * speed) * rand;
+        if (node.y > y) node.y -= ((node.y / node.rotation) * speed) * rand;
+        if (node.y < y) node.y += ((node.y / node.rotation) * speed) * rand;
+        if (!this.isMouseDown) {
+          this.canvasRotate += 0.0000000001;
+        }
+        this.ctx.translate(window.innerWidth / 2, window.innerHeight / 2);
+        this.ctx.rotate(this.canvasRotate);
+        this.ctx.translate(-window.innerWidth / 2, -window.innerHeight / 2);
+      }
+    });
+    requestAnimationFrame(() => this.draw());
+  };
+
+  addNode(i: number) {
+    const halfW = window.innerWidth / 2;
+    const halfH = window.innerHeight / 2;
+    const speed = 1.5;
+    const size = Math.max(0.5, speed * 0.1);
+    return {
+      speed,
+      size,
+      ordinal: i,
+      rotation: Math.floor(Math.random() * 1000) + 50,
+      x: Math.floor(Math.random() * halfW) + halfW,
+      y: Math.floor(Math.random() * halfH) + halfH
+    };
+  };
+
+  makeColor(colorNum: number, colors: number) {
+    if (colors < 1) colors = 1;
+    const val = (colorNum * (360 / colors)) % 360;
+    return 'hsl( ' + val + ', 100%, 60% )';
+  }
+
+  async setupAudio() {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
+    const audioContext = new AudioContext();
+    const mediaStreamAudioSourceNode = audioContext.createMediaStreamSource(stream);
+    const analyserNode = audioContext.createAnalyser();
+    mediaStreamAudioSourceNode.connect(analyserNode);
+
+    const pcmData = new Float32Array(analyserNode.fftSize);
+    const onFrame = () => {
+      analyserNode.getFloatTimeDomainData(pcmData);
+      let sumSquares = 0.0;
+      for (const amplitude of pcmData) { sumSquares += amplitude * amplitude; }
+      this.volumeMeterValue = Math.sqrt(sumSquares / pcmData.length);
+      window.requestAnimationFrame(onFrame);
+    };
+    window.requestAnimationFrame(onFrame);
+  }
+}
+
+export default CanvasHelper;
+
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,87 +1,14 @@
 <script lang="ts">
+	import CanvasHelper from '$lib/canvas';
 	import { onMount } from 'svelte';
 
 	let canvas: HTMLCanvasElement;
-	let mousePosition = { x: 0, y: 0 };
-	let isMouseDown = false;
-	let ticker = 0;
-	let canvasRotate = Math.PI / 10;
+	let canvasHelper: CanvasHelper;
+
+	let wheelValue = 1;
 
 	onMount(() => {
-		canvas.width = window.innerWidth;
-		canvas.height = window.innerHeight;
-		const ctx = canvas.getContext('2d');
-
-		function makeColor(colorNum: number, colors: number) {
-			if (colors < 1) colors = 1;
-			// defaults to one color - avoid divide by zero
-			return (colorNum * (360 / colors)) % 360;
-		}
-
-		function addNode(i: number) {
-			var color = 'hsl( ' + makeColor(i, 20) + ', 100%, 60% )';
-			const halfW = window.innerWidth / 2;
-			const halfH = window.innerHeight / 2;
-			const speed = Math.floor(Math.random() * 10) / 20 + 0.5;
-			const size = speed * 1.2;
-			return {
-				color,
-				speed,
-				size,
-				rotation: Math.floor(Math.random() * 1000) + 50,
-				x: Math.floor(Math.random() * halfW) + halfW,
-				y: Math.floor(Math.random() * halfH) + halfH
-			};
-		}
-
-		const nodes = [...new Array(2000)].map((_, i) => addNode(i));
-
-		const draw = () => {
-			const { x, y } = mousePosition;
-			if (ctx && ticker < 1) {
-				ticker += 0.05;
-				ctx.globalAlpha = ticker;
-			}
-			ctx?.resetTransform();
-			ctx?.clearRect(0, 0, window.innerWidth, window.innerHeight);
-			nodes.forEach((node) => {
-				if (ctx) {
-					ctx.beginPath();
-					ctx.arc(node.x, node.y, node.size, 0, 2 * Math.PI);
-					ctx.fillStyle = node.color;
-					ctx.fill();
-					if (node.x > x) node.x -= (node.x / node.rotation) * node.speed;
-					if (node.x < x) node.x += (node.x / node.rotation) * node.speed;
-					if (node.y > y) node.y -= (node.y / node.rotation) * node.speed;
-					if (node.y < y) node.y += (node.y / node.rotation) * node.speed;
-					if (!isMouseDown) {
-						canvasRotate += 0.0000000001;
-					}
-					ctx?.translate(window.innerWidth / 2, window.innerHeight / 2);
-					ctx?.rotate(canvasRotate);
-					ctx?.translate(-window.innerWidth / 2, -window.innerHeight / 2);
-				}
-			});
-			requestAnimationFrame(draw);
-		};
-		draw();
-
-		const move = (e: MouseEvent | TouchEvent) => {
-			const mouseEvent = e as MouseEvent;
-			if (mouseEvent.clientX && mouseEvent.clientY) {
-				const { clientX: x, clientY: y } = mouseEvent;
-				mousePosition = { x, y };
-				isMouseDown = mouseEvent.buttons !== 0;
-			} else {
-				e.preventDefault();
-				const touchEvent = e as TouchEvent;
-				const { clientX: x, clientY: y } = touchEvent.touches[0];
-				mousePosition = { x, y };
-			}
-		};
-
-		window.addEventListener('mousemove', move);
-		window.addEventListener('touchmove', move);
+		canvasHelper = new CanvasHelper(canvas);
 	});
 </script>
 
